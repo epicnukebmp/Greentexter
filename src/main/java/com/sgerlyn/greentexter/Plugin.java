870282f36ae270ffedaa5cf62c51f571e596e275
sgerlyn <samgerlyn@gmail.com>
2016-12-23 00:06:33 -0500

    Clean imports

--- src/main/java/com/sgerlyn/greentexter/Plugin.java
+++ src/main/java/com/sgerlyn/greentexter/Plugin.java
@@ -1,7 +1,6 @@
 package com.sgerlyn.greentexter;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
